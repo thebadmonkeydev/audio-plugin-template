1d6abb2432c59512e56ef89849cc43bb1d0d4b46
Michael Kelly <michaelkelly322@gmail.com>
2023-09-04 00:45:37 -0400

    Cleanup juce-required classes

--- plugin/include/YourPluginName/PluginEditor.h
+++ plugin/include/YourPluginName/PluginEditor.h
@@ -1,22 +1,25 @@
 #pragma once
 
+#ifndef _JUCE_PLUGIN_EDITOR_H_
+#define _JUCE_PLUGIN_EDITOR_H_
+
 #include "PluginProcessor.h"
 
 namespace audio_plugin {
+  class AudioPluginAudioProcessorEditor : public juce::AudioProcessorEditor {
+    public:
+      explicit AudioPluginAudioProcessorEditor(AudioPluginAudioProcessor &);
+      ~AudioPluginAudioProcessorEditor() override;
 
-class AudioPluginAudioProcessorEditor : public juce::AudioProcessorEditor {
-public:
-  explicit AudioPluginAudioProcessorEditor(AudioPluginAudioProcessor &);
-  ~AudioPluginAudioProcessorEditor() override;
+      void paint(juce::Graphics &) override;
+      void resized() override;
 
-  void paint(juce::Graphics &) override;
-  void resized() override;
+    private:
+      // This reference is provided as a quick way for your editor to
+      // access the processor object that created it.
+      AudioPluginAudioProcessor &processorRef;
 
-private:
-  // This reference is provided as a quick way for your editor to
-  // access the processor object that created it.
-  AudioPluginAudioProcessor &processorRef;
-
-  JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(AudioPluginAudioProcessorEditor)
-};
+      JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(AudioPluginAudioProcessorEditor)
+  };
 } // namespace audio_plugin
+#endif
